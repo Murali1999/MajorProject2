dde0e9d1359ca8689388f780b44140cd663e6830
Murali Narayan <33386752+Murali1999@users.noreply.github.com>
2021-05-04 12:06:06 +0000

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
-import socket, subprocess
+import socket
+import subprocess
 from subprocess import PIPE
-import sys, textwrap
+import sys
 import os
-import re, random
+import time
+import re
+import random
 import texttable
-import requests
 import json
 import stdiomask
+import requests
+import datetime
 from bs4 import BeautifulSoup
 
 #ascii banner for the CLI tool
@@ -56,42 +60,68 @@ group2.add_argument('-g', help='Generate the report in PDF format', action='stor
 args = parser.parse_args()
 
 site = args.s
+
+def cmdline(command):
+	process = subprocess.check_output(
+	args = command,
+	shell = True
+	)
+	return process
+
+def wappalyzer(url):
+	urls = 'https://'+url
+	api_key = 'dhVj82iF6Y81BaXZHnGEY7BH9pAxdoLraID1ePdn'
+	endpoint = "https://api.wappalyzer.com/lookup/v2/?urls="
+	cmd1 = 'curl -s -H "x-api-key: '
+	cmd2 = '" '+ endpoint + urls
+	cmd = cmd1 + api_key + cmd2
+	resource_response = subprocess.check_output(cmd, shell=True)
+	response = json.loads(resource_response)
+	ret = json.dumps(response,sort_keys=True, indent=4)
+	if "crawl" in ret:
+	       	ret = "Website being crawled, please try again in a few minutes."
+	       	return ret
+	else:
+		return ret
+
+#recon and fingerprinting option
 if (args.s):
 	print('-> Target site name: {}'.format(site))
 	print()
 
-	def cmdline(command):
-    		process = subprocess.check_output(
-        	args = command,
-        	shell = True
-    		)
-    		return process
-	
-	def wappalyzer(url):
-		urls = 'https://'+url
-		api_key = 'dhVj82iF6Y81BaXZHnGEY7BH9pAxdoLraID1ePdn'
-		endpoint = "https://api.wappalyzer.com/lookup/v2/?urls="
-		cmd1 = 'curl -s -H "x-api-key: '
-		cmd2 = '" '+ endpoint + urls
-		cmd = cmd1 + api_key + cmd2
-		resource_response = subprocess.check_output(cmd, shell=True)
-		response = json.loads(resource_response)
-		ret = json.dumps(response,sort_keys=True, indent=4)
-		if "crawl" in ret:
-	        	ret = "Website being crawled, please try again in a few minutes."
-	        	return ret
-		else:
-	        	return ret
-	
 	#ret=cmdline('nslookup {} && dig +noall +answer +multiline txt {}'.format(site, site)).decode('ascii')
 	
 	ret1=cmdline('dnsrecon -d {}'.format(site)).decode('ascii')
+	print(ret1)
+	os.system('clear')
+	print('(1/7) DNS Lookup completed..... ')
 	ret2=cmdline('dig @8.8.8.8 +nocmd {} any +multiline +noall +answer'.format(site)).decode('ascii')
+	print(ret2)
+	os.system('clear')
+	print('(2/7) DNS Enumeration completed.....')
 	ret3=cmdline('python3 /root/Desktop/major1/cms-detector.py -s {}'.format(site)).decode('ascii')
+	print(ret3)
+	os.system('clear')
+	print('(3/7) CMS Detection completed.....')
 	ret4=cmdline('nmap -O {}'.format(site)).decode('ascii')
+	print(ret4)
+	os.system('clear')	
+	print('(4/7) OS Fingerprinting completed.....')
 	ret5=cmdline('uniscan -u {} -e | grep "Scan date:" -A30'.format(site)).decode('ascii')
+	print(ret5)
+	os.system('clear')
+	print('(5/7) Static Checks completed.....')
 	ret6=cmdline('uniscan -u {} -g | grep "Scan date:" -A800'.format(site)).decode('ascii')
+	print(ret6)
+	os.system('clear')
+	print('(6/7) Web Fingerprinting completed.....')
 	ret7=wappalyzer(site)
+	print(ret7)
+	os.system('clear')
+	print('(7/7) Technologies used found.....')
+	print('All scans are completed, generating report now.')
+	time.sleep(2)
+	os.system('clear')
 	
 	table = texttable.Texttable()
 	table.set_cols_align(["c", "l"])
@@ -104,11 +134,14 @@ if (args.s):
 		["OS Fingerprinting:", ret4],
 		["Static Checks and Robots.txt and Sitemap.xml Check:", ret5],
 		["Web fingerprinting:", ret6],
-		["Technologies used:", ret7]])
+		["Technologies Used:", ret7]])
 	print(table.draw() + "\n")
-	
+
+#google dorking option
 query = args.d
 if (args.d):
+	print('Getting google dork results....')
+	os.system('clear')
 	print('Google Dork Results:')
 	print()
 	query = query.replace(' ','+')
@@ -125,3 +158,51 @@ if (args.d):
 		links = soup.findAll("a")
 		for link in  soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
     			print(re.split(":(?=http)",link["href"].replace("/url?q=","")))
+
+#pdf generation option
+if (args.g):
+	from datetime import date
+	today=date.today()
+	now=today.strftime('%A, %B %d, %Y')
+
+	pdf = FPDF()
+	
+	pdf.add_page()
+	pdf.set_font("Arial",size=28)
+	pdf.multi_cell(190,90,txt='         ',align='J')
+	pdf.multi_cell(190,18,txt='Reconnaissance &',align='C')
+	pdf.multi_cell(190,18,txt='Fingerprinting',align='C')
+	pdf.multi_cell(190,18,txt='Report',align='C')
+	pdf.multi_cell(190,18,txt=now,border=1,align='C')
+
+	pdf.add_page()
+	pdf.set_font("Arial",size=8.5)
+	pdf.multi_cell(200,30,txt='         ',align='J')
+	pdf.cell(50,5, txt='DNS Lookup and Records:',align='L')  
+	pdf.multi_cell(200,5,txt=ret1,align='L')
+	pdf.multi_cell(200,5,txt='         ',align='J')
+	pdf.cell(50,5, txt='DNS Enumeration:', align='L')
+	pdf.multi_cell(200,5,txt=ret2,align='L')
+
+	pdf.add_page()
+	pdf.set_font("Arial",size=8.5)
+	pdf.cell(30,5, txt='CMS Detection:',align='L')
+	pdf.multi_cell(200,5,txt=ret3,align='L')
+	pdf.multi_cell(200,10,txt='         ',align='J')
+	pdf.cell(30,5, txt='OS Fingerprinting:',align='L')  
+	pdf.multi_cell(200,5,txt=ret4,align='L')
+	pdf.multi_cell(200,20,txt='         ',align='J')
+	pdf.cell(30,5, txt='Static Checks:',align='L')  
+	pdf.multi_cell(200,5,txt=ret5,align='L')
+
+	pdf.add_page()
+	pdf.set_font("Arial",size=9)
+	pdf.cell(30,5, txt='Web Fingerprinting:',align='L')
+	pdf.multi_cell(200,5,txt=ret6,align='L')
+
+	pdf.add_page()
+	pdf.set_font("Arial",size=9)
+	pdf.cell(30,5, txt='Technologies used:',align='L')
+	pdf.multi_cell(200,5,txt=ret7,align='L')
+
+	pdf.output("myresult.pdf")
